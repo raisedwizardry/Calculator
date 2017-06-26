aeb0bf3a4b27ce9acac17f8d193b7408ad6a496b
Eli Davis <Eli Davis>
2017-06-26 07:48:38 -0400

    Add project files.

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,58 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+public interface IStateManager<Quo>
+{
+    void Save(string name, Quo state);
+    Quo Load(string name);
+}
+
+public class SessionStateManager<Quo> : IStateManager<Quo>
+{
+    public void Save(string name, Quo state)
+    {
+        HttpContext.Current.Session[name] = state;
+    }
+    public Quo Load(string name)
+    {
+        return (Quo)HttpContext.Current.Session[name];
+    }
+}
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        protected IStateManager<CalculatorModel> stateManager = new SessionStateManager<CalculatorModel>();
+        public void SetStateManager(IStateManager<CalculatorModel> manager)
+        {
+            stateManager = manager;
+        }
+
+        public ActionResult Index()
+        {
+            CalculatorModel calculator = new CalculatorModel();
+            stateManager.Save("model", calculator);
+
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string param, string operate)
+        {
+            CalculatorModel calculator = stateManager.Load("model");
+
+            if (param != null)
+                calculator.Process(param);
+            else if (operate != null)
+                calculator.DoOperate(operate);
+            stateManager.Save("model", calculator);
+
+            return View("Index", calculator);
+        }
+    }
+}
---
+++ Calculator/Models/CalculatorModel.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    public class CalculatorModel
+    {
+        public string Display { get; set; }
+        public bool clearDisplay;
+        public string First { get; set; }
+        public string Second { get; set; }
+        public string Operate { get; set; }
+        public string LastOperate { get; set; }
+        public string disabled;
+        public bool frozen;
+        public string result;
+
+        public CalculatorModel()
+        {
+            Clear();
+        }
+        
+        public void Clear()
+        {
+            Display = "0";
+            clearDisplay = false;
+            frozen = false;
+            result = string.Empty;
+            LastOperate = string.Empty;
+            disabled= string.Empty;
+            First = string.Empty;
+            Second = string.Empty;
+            Operate = string.Empty;
+        }
+
+        public void Process(string press)
+        {
+            switch (press)
+            {
+                case "+/-":
+                    if (Display == "0")
+                        break;
+                    else if (Display.Contains("-"))
+                    {
+                        Display = Display.Remove(Display.IndexOf("-"), 1);
+                    }
+                    else
+                        Display = "-" + Display;
+                    break;
+
+                case ".":
+                    var d = NumberFormatInfo.CurrentInfo.NumberDecimalSeparator;
+                    if (Display == "0")
+                        Display = "0" + d;
+                    else
+                    {
+                        if (!Display.Contains(d))
+                            Display = Display + d;
+                    }
+                    break;
+
+                case "C":
+                    Clear();
+                    break;
+
+                default:
+                    if (Display == "0" || clearDisplay)
+                        Display = press;
+                    else
+                        Display = Display + press;
+                    break;
+            }
+            if (press != ".")
+                clearDisplay = false;
+        }
+
+        private void DoResult()
+        {
+            double tempResult;
+
+            switch (Operate)
+            {
+                case ("+"):
+                    tempResult = Convert.ToDouble(First) + Convert.ToDouble(Second);
+                    break;
+
+                case ("-"):
+                    tempResult = Convert.ToDouble(First) - Convert.ToDouble(Second);
+                    break;
+
+                case ("*"):
+                    tempResult = Convert.ToDouble(First) * Convert.ToDouble(Second);
+                    break;
+
+                case ("/"):
+                    tempResult = Convert.ToDouble(First) / Convert.ToDouble(Second);
+                    break;
+
+                case ("sqrt"):
+                    tempResult = Math.Sqrt(Convert.ToDouble(First));
+                    break;
+
+                case ("%"):
+                    tempResult = Convert.ToDouble(First) * Convert.ToDouble(Second) / 100;
+                    break;
+
+                default:
+                    tempResult = Double.NaN;
+                    break;
+            }
+            result = tempResult.ToString();
+            Display = result;
+            if (Double.IsNaN(tempResult) || Double.IsNegativeInfinity(tempResult) || Double.IsPositiveInfinity(tempResult))
+            {
+                frozen = true;
+                disabled = "disable";
+                Display = "ERR";
+            }
+        }
+        public void DoOperate(string operate)
+        {
+            if (First == string.Empty || (clearDisplay && LastOperate != "="))
+            {
+                First = Display;
+                LastOperate = operate;
+                clearDisplay = true;
+            }
+            else
+            {
+                Second = Display;
+                Operate = LastOperate;
+                DoResult();
+                LastOperate = operate;
+                First = result;
+                clearDisplay = true;
+            }
+
+            if (operate == "=")
+            {
+                First = string.Empty;
+                Second = string.Empty;
+                Operate = string.Empty;
+            }
+            else if (operate == "sqrt")
+            {
+                First = Display;
+                Operate = operate;
+                DoResult();
+                LastOperate = operate;
+                First = string.Empty;
+                clearDisplay = true;
+            }
+        }
+    }
+}
